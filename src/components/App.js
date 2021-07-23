f92e131a9b05d75dd80db24e8e13fc9f54e38ef9
Alex-A-B <alexjamesbelton@gmail.com>
2021-07-23 21:31:10 +0100

    displays 4 sushi and gets the next 4 sushi on click

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
+
 function App() {
+  const [getSushi, setGetSushi] = useState([])
+
+useEffect( () => {
+fetch(API)
+.then(response => response.json())
+.then(data => setGetSushi(data))
+.catch(error => console.log("server not working - start it up"))
+}, [])
+
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={getSushi} />
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer( { sushis }) {
+//sushis is all 100 sushi
+// sushi have ID 1-100
+// only require 4 at a time to render
+// filter/splice to new array?
+  const [sushiID, setSushiID] = useState(1)
+
+  const start = sushiID
+  const end = (sushiID + 4)
+
+  const beltDishes = sushis.filter((sushi) => {return sushi.id >= start && sushi.id < end})
+
+  function handleMoreClick() {
+    setSushiID(end)
+  }
+
+  console.log(beltDishes)
+
+  const renderSushi = beltDishes.map((dish) => {
+    return (
+      <Sushi
+          key={dish.id}
+          image={dish.img_url}
+          name={dish.name}
+          price={dish.price}
+      />
+    )
+  })
+
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushi}
+      <MoreButton moreSushi={handleMoreClick} />
     </div>
   );
 }
